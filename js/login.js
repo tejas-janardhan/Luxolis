dd87da89eab0c1fe42944e237d896a74eefb4f31
tejasj1718 <55607809+tejasj1718@users.noreply.github.com>
2023-09-14 20:34:16 +0530

    inital commit

---
+++ js/login.js
@@ -0,0 +1,40 @@
+const DUMMY_USERNAME = "test@luxpmsoft.com";
+const DUMMY_PASSWORD = "test1234!";
+
+function loginHandler(username, password) {
+  if (username !== "" && password !== "") {
+    if (username !== DUMMY_USERNAME) {
+      alert("Username does not exist");
+      return;
+    }
+    if (password !== DUMMY_PASSWORD) {
+      alert("Wrong combination");
+      return;
+    }
+    window.location.href = "about:blank";
+  } else {
+    alert("Password or username empty");
+  }
+}
+
+function passwordInputHandler(event) {
+  event.target.value = event.target.value.replace(/\s/g, "");
+}
+
+function main() {
+  const passwordElement = document.getElementById("password");
+  const usernameElement = document.getElementById("username");
+
+  const loginElement = document.getElementById("login-button");
+
+  passwordElement.addEventListener("input", passwordInputHandler);
+
+  loginElement.addEventListener("click", function (event) {
+    event.preventDefault();
+    loginHandler(usernameElement.value, passwordElement.value);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  main();
+});
